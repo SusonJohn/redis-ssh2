ec779ebfe4eb751ec15c806bdb16068e58aa945b
Suson <591016130@qq.com>
2021-11-02 16:10:29 +0800

    fix to support privateKey in server and redis

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const Client = require('ssh2').Client;
 const net = require('net');
 const fs = require('fs');
 
-function connectToSSH (options) {
+function connectToSSH(options) {
     return new Promise((resolve, reject) => {
         const connection = new Client();
         connection.once('ready', () => resolve(connection));
@@ -32,50 +32,79 @@ function createIntermediateServer(connectionListener) {
     });
 }
 async function connect(sshConfig, redisConfig) {
-        let redis, server, sshConnection
-        try {
-            sshConnection = await connectToSSH({
-                host: sshConfig.host,
-                port: sshConfig.port,
-                username: sshConfig.user,
-                password: sshConfig.password
-            });
-        } catch (err) {
-            return new Error(`sshConnection failed ${err}`)
+    let redis, server, sshConnection, sshDefualt
+    sshDefualt = {
+        host: sshConfig.host,
+        port: sshConfig.port,
+        username: sshConfig.user
+    }
+    if (sshConfig.password) {
+        sshDefualt = {
+            ...sshDefualt,
+            password: sshConfig.password
         }
-        tunnel._conn=sshConnection
-        try {
-            server = await createIntermediateServer(socket => {
-                sshConnection.forwardOut(
-                    socket.remoteAddress,
-                    socket.remotePort,
-                    redisConfig.host,
-                    redisConfig.port,
-                    (error, stream) => {
-                        if (error) {
-                            socket.end();
-                        } else {
-                            socket.pipe(stream).pipe(socket);
-                        }
+    }
+    if (sshConfig.privateKey) {
+        sshDefualt = {
+            ...sshDefualt,
+            privateKey: sshConfig.privateKey
+        }
+    }
+    try {
+        sshConnection = await connectToSSH({
+            ...sshDefualt
+        });
+    } catch (err) {
+        throw new Error(`sshConnection failed ${err}`)
+    }
+    tunnel._conn = sshConnection
+    try {
+        server = await createIntermediateServer(socket => {
+            sshConnection.forwardOut(
+                socket.remoteAddress,
+                socket.remotePort,
+                redisConfig.host,
+                redisConfig.port,
+                (error, stream) => {
+                    if (error) {
+                        socket.end();
+                    } else {
+                        socket.pipe(stream).pipe(socket);
                     }
-                );
-            });
-        } catch (err) {
-            return new Error(`createIntermediateServer failed ${err}`)
+                }
+            );
+        });
+    } catch (err) {
+        throw new Error(`createIntermediateServer failed ${err}`)
+    }
+    let sshRedis
+    sshRedis = {
+        host: server.address().address,
+        port: server.address().port,
+        db: redisConfig.db
+    }
+    if (redisConfig.password) {
+        sshRedis = {
+            ...sshRedis,
+            password: redisConfig.password
         }
-        try {
-            // Redis specify the address of the intermediate server, not the server
-            redis = await connectToRedis({
-                host: server.address().address,
-                port: server.address().port,
-                password: redisConfig.password,
-                db: redisConfig.db
-            });
-            tunnel._redis=redis
-            return redis
-        } catch (err) {
-            return new Error(`connectToRedis failed ${err}`)
+    }
+    if (redisConfig.privateKey) {
+        sshRedis = {
+            ...sshRedis,
+            privateKey: redisConfig.privateKey
         }
+    }
+    try {
+        // Redis specify the address of the intermediate server, not the server
+        redis = await connectToRedis({
+            ...sshRedis
+        });
+        tunnel._redis = redis
+        return redis
+    } catch (err) {
+        throw new Error(`connectToRedis failed ${err}`)
+    }
 }
 
 var tunnel = module.exports = {
@@ -94,7 +123,7 @@ var tunnel = module.exports = {
      * @return Promise <Redis and ssh connection>
      */
     connect: function (sshConfig, redisConfig) {
-        return new Promise ((resolve,reject)=>{
+        return new Promise((resolve, reject) => {
             connect(sshConfig, redisConfig).then(
                 redis=>{
                     resolve(redis)
@@ -104,11 +133,11 @@ var tunnel = module.exports = {
             })
         })
     },
-    close: function(){
-        if('end' in tunnel._redis){
+    close: function () {
+        if ('end' in tunnel._redis) {
             tunnel._redis.end()
         }
-        if('end' in tunnel._conn){
+        if ('end' in tunnel._conn) {
             tunnel._conn.end()
         }
     }
